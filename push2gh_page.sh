5efe119bf392e8479b81c2ad43ccbcde73023308
ThMRudolf <th.m.rudolf@gmail.com>
2024-07-28 17:19:22 -0600

    bash script to updated gh-pages, needs to be checked

---
+++ push2gh_page.sh
@@ -0,0 +1,7 @@
+cd _site
+git init
+git remote add origin https://github.com/ThMRudolf/Automat-and-Industrial-Robots.git
+git checkout -b gh-pages
+git add .
+git commit -m "Deploy site" 
+git push -f origin gh-pages
